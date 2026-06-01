2ba8814742c1eb97031962e4f1008800fa7c0349
JudahBudah <judajbudaj@gmail.com>
2026-06-02 01:24:52 +0800

    Login & Register styling

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import api from '../api/axios'
+import { MdOutlineEmail } from "react-icons/md";
+import { MdLockOutline } from "react-icons/md";
 
 function Login() {
   const [email, setEmail] = useState('')
@@ -20,24 +22,39 @@ function Login() {
   }
 
   return (
-    <div>
-      <h1>Login</h1>
-      {error && <p>{error}</p>}
-      <form onSubmit={handleSubmit}>
-        <input
-          type='email'
-          placeholder='Email'
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type='password'
-          placeholder='Password'
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type='submit'>Login</button>
-      </form>
+    <div className='bg-zinc-900 flex items-center justify-center h-screen text-white'>
+      <div className='bg-zinc-800 p-8 px-14 rounded-lg shadow-md w-full max-w-md flex flex-col gap-4 items-center'>
+        <h1 className='text-center text-3xl font-bold'>Login</h1>
+        {error && <p className='text-red-600'>{error}</p>}
+        <form onSubmit={handleSubmit} className='flex flex-col gap-4 items-center w-full'>
+          <div className='w-full'>
+            <MdOutlineEmail className='absolute text-2xl text-zinc-400 mt-2 ml-2 z-10' />
+            <input
+              type='email'
+              placeholder='Email'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className='bg-zinc-700 p-2 pl-10 rounded-md outline-0 w-full relative'
+            />
+          </div>
+          <div className='w-full'>
+            <MdLockOutline className='absolute text-2xl text-zinc-400 mt-2 ml-2 z-10' />
+            <input
+              type='password'
+              placeholder='Password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className='bg-zinc-700 p-2 pl-10 rounded-md outline-0 w-full relative'
+            />
+          </div>
+          <button type='submit' className='bg-orange-500 w-60 text-[1.2rem] p-1 rounded-md cursor-pointer items-center mt-2'>Login</button>
+        </form>
+        <p>New user?
+          <a onClick={() => navigate('/register')} className='cursor-pointer text-blue-600 text-center pl-2'>
+            Create an account here
+          </a>
+        </p>
+      </div>
     </div>
   )
 }
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,6 +1,9 @@
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import api from '../api/axios'
+import { FaRegUser } from "react-icons/fa";
+import { MdOutlineEmail } from "react-icons/md";
+import { MdLockOutline } from "react-icons/md";
 
 function Register() {
   const [username, setUsername] = useState('')
@@ -20,33 +23,54 @@ function Register() {
   }
   
   return (
-    <div>
-      <h1>Register</h1>
-      {error && <p>{error}</p>}
-      <form onSubmit={handleSubmit}>
-        <input
-          type='text'
-          placeholder='Username'
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <input
-          type='email'
-          placeholder='Email'
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type='password'
-          placeholder='Password'
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type='submit'>Register</button>
-      </form>
+    <div className='bg-zinc-900 flex items-center justify-center h-screen text-white'>
+      <div className='bg-zinc-800 p-8 px-14 rounded-lg shadow-md w-full max-w-md flex flex-col gap-4 items-center'>
+        <h1 className='text-center text-3xl font-bold'>Register</h1>
+
+        {error && <p className='text-red-600'>{error}</p>}
+
+        <form onSubmit={handleSubmit} className='flex flex-col gap-4 items-center w-full'>
+          <div className='w-full'>
+            <FaRegUser className='absolute text-[1.25rem] text-zinc-400 mt-2.5 ml-2.5 z-10' />
+            <input
+              type='text'
+              placeholder='Username'
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              className='bg-zinc-700 p-2 pl-10 rounded-md outline-0 w-full relative'
+            />
+          </div>
+          <div className='w-full'>
+            <MdOutlineEmail className='absolute text-2xl text-zinc-400 mt-2 ml-2 z-10' />
+            <input
+              type='email'
+              placeholder='Email'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className='bg-zinc-700 p-2 pl-10 rounded-md outline-0 w-full relative'
+            />
+          </div>
+          <div className='w-full'>
+            <MdLockOutline className='absolute text-2xl text-zinc-400 mt-2 ml-2 z-10' />
+            <input
+              type='password'
+              placeholder='Password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className='bg-zinc-700 p-2 pl-10 rounded-md outline-0 w-full relative'
+            />
+          </div>
+
+          <button type='submit' className='bg-orange-500 w-60 text-[1.2rem] p-1 rounded-md cursor-pointer items-center mt-2'>Register</button>
+        </form>
+        <p>Already a user?
+          <a onClick={() => navigate('/')} className='cursor-pointer text-blue-600 text-center pl-2'>
+            Sign-in here
+          </a>
+        </p>
+      </div>
     </div>
   )
-  
 }
 
 export default Register
