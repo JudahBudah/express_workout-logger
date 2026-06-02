a4ac856004421ca16f0120f2b8822d1323528059
JudahBudah <judajbudaj@gmail.com>
2026-06-03 02:26:06 +0800

    Updated baseURL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000'
+  baseURL: 'https://expressworkout-logger-production.up.railway.app'
 })
 
 api.interceptors.request.use((config) => {
